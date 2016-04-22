ea3ba8f2be5f58d9b98a15f921e6286bfc71b25a
sout0053 <maxwell.southwood@outlook.com>
2016-04-22 10:24:21 -0400

    Added waypoints

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var $calories = $('.calories')
+var $know = $('.know')
+var $moderation = $('.moderation')
+
+$calories.waypoint(function (direction) {
+  if (direction == 'down') {
+    $calories.addClass('calories-svg-2-animate');
+  } else {
+    $calories.removeClass('calories-svg-2-animate');
+  }
+}, { offset: '50%'});
+
+$know.waypoint(function (direction) {
+  if (direction == 'down') {
+    $know.addClass('know-animate');
+  } else {
+    $know.removeClass('know-animate');
+  }
+}, { offset: '50%'});
+
+$moderation.waypoint(function (direction) {
+  if (direction == 'down') {
+    $moderation.addClass('moderation-animate');
+  } else {
+    $moderation.removeClass('moderation-animate');
+  }
+}, { offset: '50%'});
